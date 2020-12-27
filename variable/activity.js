443f3583cc1f6a6f26e89e220fd11d5938c2c354
Raquel Ribeiro <raquel.ribeiro354@outlook.com>
2020-12-27 20:39:29 -0300

    completion of activity.js

---
+++ variable/activity.js
@@ -0,0 +1,53 @@
+// 01 - create variables and constants with all kinds of data we have in javascript in a file called activity.js
+// Activit 1 - string cons and let
+
+const myName = "Raquel";
+console.log(myName);
+
+let yourName = "Vitor";
+console.log(yourName);
+
+// Activit 2 - nunber cons and let
+
+const age = 19;
+console.log(age);
+
+let year = 2021;
+console.log(year);
+
+// Activit 3 - boolean cons and let
+
+const goBeach = false;
+const stayHome = true;
+console.log(goBeach);
+
+let beach = true;
+let home = false;
+console.log(beach);
+
+// Activit 4 - array cons and let
+
+const guestsBeach = [
+  { name: "Raquel" },
+  { name: "Vitor" },
+  { name: "M. Eduarda" },
+  { name: "Hebert" },
+  { name: "Felipe" },
+];
+console.log(guestsBeach[3]);
+
+let frut = ["Maça", "Banana", "Uva", "Pera"];
+console.log(frut[1]);
+
+// Activit 5 - Object cons and let
+
+const carBestSellers = [
+  { model: "Gol", brand: "Volkswagen", year: 2020 },
+  { model: "Uno", brand: "Fiat", year: 2018 },
+  { model: "HB20", brand: "Hyundai", year: 2015 },
+];
+console.log(carBestSellers[2]);
+
+let car = { model: "Gol", brand: "Volkswagen", year: 2020 };
+
+console.log(car);
